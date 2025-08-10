00e92e17cacb5f989d908ee557b7452d8074d985
m-gail <34513390+m-gail@users.noreply.github.com>
2025-08-10 16:06:22 +0200

    Redirect on index page

---
+++ src/app/pages/Index/IndexPage.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useSettingsStore } from '@/app/store/settings'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+const settings = useSettingsStore()
+
+if (settings.directoryPath === '') {
+  router.push('/settings')
+} else {
+  router.push('/agenda')
+}
+</script>
--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import AgendaPage from '../pages/Agenda/AgendaPage.vue'
 import SettingsPage from '../pages/Settings/SettingsPage.vue'
+import IndexPage from '../pages/Index/IndexPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    { path: '/', component: IndexPage },
     { path: '/agenda', component: AgendaPage },
     { path: '/settings', component: SettingsPage },
   ],
