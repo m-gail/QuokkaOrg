8a4ff3d3630ae306982d6f0139e92604c4f4cb03
m-gail <34513390+m-gail@users.noreply.github.com>
2026-02-23 20:39:59 +0100

    Add separators to breadcrumbs

--- src/components/Breadcrumbs.vue
+++ src/components/Breadcrumbs.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts">
+import ChevronRightIcon from './icons/ChevronRightIcon.vue';
+
 const { breadcrumbs } = defineProps<{ breadcrumbs: string[] }>()
 </script>
 
 <template>
-  <div class="row breadcrumbs">
-    <span v-for="breadcrumb in breadcrumbs" :key="breadcrumb">
-      {{ breadcrumb }}
-    </span>
+  <div class="breadcrumbs">
+    <div v-for="(breadcrumb, index) in breadcrumbs" :key="breadcrumb" class="breadcrumb">
+      <span>{{ breadcrumb }}</span>
+      <i v-if="index != breadcrumbs.length - 1" class="separator"><ChevronRightIcon /></i>
+    </div>
   </div>
 </template>
 
 <style lang="css" scoped>
 .breadcrumbs {
+  display: flex;
+  flex-wrap: wrap;
   overflow: auto;
 }
 </style>
