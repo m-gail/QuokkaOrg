b2c4424d6272a182e7f0ec5183cddfef983c098d
m-gail <34513390+m-gail@users.noreply.github.com>
2026-02-23 20:39:39 +0100

    Add colors for Card badges

--- src/app/common/components/AgendaDayCard.vue
+++ src/app/common/components/AgendaDayCard.vue
@@ -3,9 +3,9 @@ import Card from '@/components/Card.vue'
 import type { AgendaDay } from '@/org/types'
 import { formatDateWithWeekDay, getDayDiff } from '../date'
 import AgendaEventsList from './AgendaEventsList.vue'
-import { computed } from 'vue';
+import { computed } from 'vue'
 
-const { day, today } = defineProps<{ day: AgendaDay, today: Date }>()
+const { day, today } = defineProps<{ day: AgendaDay; today: Date }>()
 const date = computed(() => new Date(Date.parse(day.date)))
 const text = computed(() => {
   const dayDiff = getDayDiff(today, date.value)
@@ -17,10 +17,19 @@ const text = computed(() => {
     return `In ${dayDiff} days`
   }
 })
+const badgeColor = computed(() => {
+  const dayDiff = getDayDiff(today, date.value)
+  if (dayDiff <= 1) {
+    return 'primary'
+  } else if (dayDiff <= 7) {
+    return 'secondary'
+  }
+  return 'tertiary'
+})
 </script>
 
 <template>
-  <Card :title="formatDateWithWeekDay(date)" :badge="text">
+  <Card :title="formatDateWithWeekDay(date)" :badge="text" :badge-color="badgeColor">
     <AgendaEventsList :events="day.events" />
   </Card>
 </template>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
-const { title, badge } = defineProps<{ title: string; badge?: string }>()
+import { cls } from '@/app/common/classes'
+import { computed } from 'vue'
+
+const { title, badge, badgeColor } = defineProps<{
+  title: string
+  badge?: string
+  badgeColor: 'primary' | 'secondary' | 'tertiary'
+}>()
+
+const classes = computed(() =>
+  cls([badgeColor, 'chip']),
+)
 </script>
 
 <template>
   <article>
     <div class="row">
-      <h5 class="small">{{ title }}</h5>
-      <button v-if="badge != null" class="chip primary">{{ badge }}</button>
+      <h5 class="small max">{{ title }}</h5>
+      <button v-if="badge != null" :class="classes">{{ badge }}</button>
     </div>
     <p><slot /></p>
   </article>
