6bf750b5e6bd9b2f38e74d25f61c8c1bab2ae671
m-gail <34513390+m-gail@users.noreply.github.com>
2026-02-28 12:27:33 +0100

    Invalidate old store versions

--- src/app/store/agenda.ts
+++ src/app/store/agenda.ts
@@ -1,4 +1,4 @@
-import { defineStore } from 'pinia'
+import { defineStore, type StateTree } from 'pinia'
 import type { AgendaCache, ListDirCache } from '../common/agenda/cache/types'
 import { filterAgenda, type AgendaFilter } from '@/org/filter/filterAgenda'
 import { sortAgenda } from '@/org/sort'
@@ -7,22 +7,30 @@ import { updateAgendaCache } from '../common/agenda/cache/updateAgendaCache'
 import { updateListDirCache } from '../common/agenda/cache/updateListDirCache'
 import { rescheduleNotifications } from '../common/agenda/notifications/schedule'
 import { now } from '../common/date'
+import { destr } from 'destr'
 
 type AgendaStore = {
   listDirCache: ListDirCache | null
   agendaCache: AgendaCache
   timestamp: number
   updating: boolean
+  storeVersion: number
+}
+
+const CURRENT_STORE_VERSION = 1
+
+function getDefaultState(): AgendaStore {
+  return {
+    storeVersion: CURRENT_STORE_VERSION,
+    agendaCache: { knownFiles: [], cachedAgenda: { days: [] } },
+    listDirCache: null,
+    timestamp: 0,
+    updating: false,
+  }
 }
 
 export const useAgendaStore = defineStore('agenda', {
-  state: () =>
-    ({
-      agendaCache: { knownFiles: [], cachedAgenda: { days: [] } },
-      listDirCache: null,
-      timestamp: 0,
-      updating: false
-    }) as AgendaStore,
+  state: () => getDefaultState(),
   getters: {
     getAgenda: (state) => {
       return (filters?: AgendaFilter) => {
@@ -33,7 +41,7 @@ export const useAgendaStore = defineStore('agenda', {
   actions: {
     async tryTriggerUpdate(directory: string, ignoredFolders: string[], force: boolean = false) {
       if ((!isOutdated(this.timestamp) || directory == '') && !force) {
-        return;
+        return
       }
       this.updating = true
       this.timestamp = now().getTime()
@@ -66,9 +74,21 @@ export const useAgendaStore = defineStore('agenda', {
       this.agendaCache = { knownFiles: [], cachedAgenda: { days: [] } }
       this.listDirCache = null
       this.timestamp = 0
-    }
+    },
   },
-  persist: true,
+  persist: {
+    serializer: {
+      serialize: JSON.stringify,
+      deserialize: (value: string): StateTree => {
+        const parsed: StateTree = destr(value)
+        if (parsed.storeVersion == null || parsed.storeVersion < CURRENT_STORE_VERSION) {
+          console.log('Old store version, discarding')
+          return getDefaultState()
+        }
+        return parsed
+      },
+    },
+  },
 })
 
 function isOutdated(timestamp: number) {
