e5db28aa454ee64f3e4e325ec4006da2237fbb34
m-gail <34513390+m-gail@users.noreply.github.com>
2026-03-17 19:18:54 +0100

    Add grayed out days of next month to calendar

--- src/components/calendar/page.ts
+++ src/components/calendar/page.ts
@@ -4,13 +4,14 @@ export function getPageItems(page: Date) {
   const days: Date[] = []
   addItemsOfPreviousMonth(page, days)
   addItemsOfCurrentMonth(page, days)
+  addItemsOfNextMonth(page, days)
   return days
 }
 
 function addItemsOfPreviousMonth(page: Date, days: Date[]) {
-  const weekdasOfPreviousMonth = (page.getDay() - 1 + 7) % 7
-  const firstVisibleDayOfPreviousMonth = getPreviousDay(page, weekdasOfPreviousMonth)
-  for (let i = 0; i < weekdasOfPreviousMonth; i++) {
+  const weekdaysOfPreviousMonth = (page.getDay() - 1 + 7) % 7
+  const firstVisibleDayOfPreviousMonth = getPreviousDay(page, weekdaysOfPreviousMonth)
+  for (let i = 0; i < weekdaysOfPreviousMonth; i++) {
     days.push(getNextDay(firstVisibleDayOfPreviousMonth, i))
   }
 }
@@ -18,6 +19,14 @@ function addItemsOfPreviousMonth(page: Date, days: Date[]) {
 function addItemsOfCurrentMonth(page: Date, days: Date[]) {
   let iter = new Date(page)
   while (iter.getMonth() == page.getMonth()) {
+    days.push(iter)
+    iter = getNextDay(iter)
+  }
+}
+
+function addItemsOfNextMonth(page: Date, days: Date[]) {
+  let iter = getNextDay(new Date(days[days.length - 1]))
+  while (iter.getDay() != 1) {
     days.push(iter)
     iter = getNextDay(iter)
   }
