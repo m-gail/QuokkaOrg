4664e60c516439e860636608adb495cbe3e11efb
m-gail <34513390+m-gail@users.noreply.github.com>
2026-03-27 15:07:26 +0100

    Validate startTime, if endTime is present

--- src/app/pages/New/validation.ts
+++ src/app/pages/New/validation.ts
@@ -4,6 +4,10 @@ export function notEmpty(ref: Ref<string>): Ref<string | undefined> {
   return computed(() => notEmptyValidator(ref.value))
 }
 
+export function notEmptyIfEndTimePresent(startTime: Ref<string>, endTime: Ref<string>): Ref<string | undefined> {
+  return computed(() => endTime.value !== '' ? notEmptyValidator(startTime.value) : undefined)
+}
+
 function notEmptyValidator(value: string): string | undefined {
   if (value === '') {
     return 'Required'
